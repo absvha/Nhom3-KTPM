7aa192992db6ae947a354b997bcef57c853d3c33
Thai-Dat <dat811098@gmail.com>
2025-12-22 17:07:12 +0700

    full project

--- Login/test_tC10.py
+++ Login/test_tC10.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 URL = "http://hauiproj.somee.com/Default.aspx"
 
@@ -19,14 +20,13 @@ class TestLoginTwoTabsSplitScreen:
     def setup_method(self):
         self.driver = webdriver.Firefox()
         self.driver.set_window_size(1600, 900)
-        self.wait = WebDriverWait(self.driver, 1)
+        self.wait = WebDriverWait(self.driver, 5)
 
     def slow(self, seconds=1):
         time.sleep(seconds)
 
     def teardown_method(self):
-        
-        time.sleep(1)          
+        time.sleep(1)
         self.driver.quit()
 
     def open_login_form(self):
@@ -44,14 +44,13 @@ class TestLoginTwoTabsSplitScreen:
         )
         user_input.clear()
         user_input.send_keys(username)
-        self.slow(2)
+        self.slow(1)
 
         pass_input = self.driver.find_element(
-            By.ID, "ContentPlaceHolder1_txtMatkhau"
-        )
+            By.ID, "ContentPlaceHolder1_txtMatkhau")
         pass_input.clear()
         pass_input.send_keys(password)
-        self.slow(2)
+        self.slow(1)
 
         self.driver.find_element(
             By.ID, "ContentPlaceHolder1_btDangnhap"
@@ -64,33 +63,34 @@ class TestLoginTwoTabsSplitScreen:
 
         # TAB 2
         self.driver.execute_script("window.open('');")
-        self.slow(1)
         self.driver.switch_to.window(self.driver.window_handles[1])
         self.open_login_form()
 
-        # TAB 1: đăng nhập user 1
+        # TAB 1 đăng nhập
         self.driver.switch_to.window(self.driver.window_handles[0])
         self.login(USER_1, PASS_1)
 
         self.wait.until(
             EC.presence_of_element_located((By.ID, "LinkDX"))
         )
-        self.slow(2)   # <<< DỪNG ĐỂ NHÌN TAB 1
+        self.slow(2)
 
-        # TAB 2: đăng nhập user 2
+        # TAB 2 đăng nhập
         self.driver.switch_to.window(self.driver.window_handles[1])
         self.login(USER_2, PASS_2)
 
-        # Kiểm tra BUG
-        logout_buttons = self.driver.find_elements(By.ID, "LinkDX")
+        # EXPECTED: PHẢI có thông báo lỗi
+        try:
+            self.wait.until(
+                EC.visibility_of_element_located(
+                    (By.ID, "ContentPlaceHolder1_txtThongbaoDN")
+                )
+            )
+            # Nếu có thông báo → test PASS (nhưng thực tế KHÔNG có)
+            assert True
 
-        self.slow(2)  # dừng để nhìn kết quả
-
-        if len(logout_buttons) > 0 and logout_buttons[0].is_displayed():
-            print("BUG CONFIRMED: Tab 2 dùng session của tab 1")
-            bug = True
-        else:
-            print("NO BUG: Tab 2 không dùng session tab 1")
-            bug = False
-
-        assert bug, "BUG: Tab 2 vẫn dùng session của tab 1"
+        except TimeoutException:
+            # KHÔNG có thông báo → FAIL 
+            pytest.fail(
+                "FAIL: Hệ thống dùng chung session cho nhiều tab, không tách phiên theo tab / request."
+            )
